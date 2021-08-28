89e7f4446f595e039049e301e6679e7f4dde1096
Moradi1412 <moradi.javid@gmail.com>
2021-08-28 11:25:50 -0700

    save functionality and local stroage

--- Assets/JavaScript/java.js
+++ Assets/JavaScript/java.js
@@ -1,13 +1,22 @@
-//getting the data from the storage 
-var list = JSON.parse(localStorage.getItem('toDoList')) || []; 
-
-var toDoList = {}; 
-//display the current date
-
 //getting the current date and display on the top
 var currentDate = moment().format('dddd MMM Do, YYYY HH:MM'); 
 $("#currentDay").html(currentDate); 
 console.log(currentDate);
+
+
+//getting the data from the storage 
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+//display the current date
+
 
 //getting only the hours for the color-coded 
 var rightNow = new Date ().getHours(); 
@@ -91,7 +100,11 @@ function timeFrame(){
 
 timeFrame();
 
-
-
-
- 
+$(".saveBtn").on("click", function() {
+    // get row text field class and row id values
+    var task = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(task);
+    // save in localStorage
+    localStorage.setItem(time,task);
+});
