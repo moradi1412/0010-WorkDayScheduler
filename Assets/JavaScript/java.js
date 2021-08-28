f44f2400b7791147a7d89ca60fb45992971eaba9
Moradi1412 <moradi.javid@gmail.com>
2021-08-28 09:45:47 -0700

    bug fixed added input group method bootstrap

--- Assets/JavaScript/java.js
+++ Assets/JavaScript/java.js
@@ -1,11 +1,22 @@
 //getting the data from the storage 
-var list = JSON.parse(localStorage.getItem('todolist')) || []; 
+var list = JSON.parse(localStorage.getItem('toDoList')) || []; 
 
+var toDoList = {}; 
 //display the current date
 
-var currentDate = moment().format('dddd MMM Do, YYYY'); 
+//getting the current date and display on the top
+var currentDate = moment().format('dddd MMM Do, YYYY HH:MM'); 
+$("#currentDay").html(currentDate); 
 //.toDate();
 
 
+
 console.log(currentDate);
-$("#currentDay").html(currentDate);  
+ 
+
+$(".saveBtn").click(function (){
+    
+var toDotask = $("#description").val(); 
+toDoList.push(toDotask);  
+localStorage.setItem('toToList', JSON.stringify(toDoList)); 
+}); 
