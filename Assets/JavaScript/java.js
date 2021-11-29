20752799f8d3fcca6a22293a8bd6e6dd5f630aa7
Moradi1412 <moradi.javid@gmail.com>
2021-11-28 20:15:49 -0800

    refactor for local stroage, change of if condition to for loop

--- Assets/JavaScript/java.js
+++ Assets/JavaScript/java.js
@@ -4,103 +4,84 @@ var currentDate = moment().format('dddd MMM Do, YYYY HH:MM');
 $("#currentDay").html(currentDate); 
 console.log(currentDate);
 
-//getting only the hours for the color-coded 
-var rightNow = new Date ().getHours(); 
-console.log(rightNow); 
-
-//If condition to color-code the rows base on the rightNow hour
-function timeFrame(){
-    if (rightNow > 9) {
-        $("#task9").addClass("past"); 
-    } else if (rightNow >= 9 && rightNow < 10 ){
-        $("#task9").addClass("present"); 
-    } else {
-        $("#task9").addClass("future"); 
-    }
-
-    if (rightNow > 10) {
-        $("#task10").addClass("past"); 
-    } else if (rightNow >= 10 && rightNow < 11 ){
-        $("#task10").addClass("present"); 
-    } else {
-        $("#task10").addClass("future"); 
-    }
-
-    if (rightNow > 11) {
-        $("#task11").addClass("past"); 
-    } else if (rightNow >= 11 && rightNow < 12 ){
-        $("#task11").addClass("present"); 
-    } else {
-        $("#task11").addClass("future"); 
-    }
-
-    if (rightNow > 12) {
-        $("#task12").addClass("past"); 
-    } else if (rightNow >= 12 && rightNow < 13 ){
-        $("#task12").addClass("present"); 
-    } else {
-        $("#task12").addClass("future"); 
-    }
-
-    if (rightNow > 13) {
-        $("#task13").addClass("past"); 
-    } else if (rightNow >= 13 && rightNow < 14 ){
-        $("#task13").addClass("present"); 
-    } else {
-        $("#task13").addClass("future"); 
-    }
-
-    if (rightNow > 14) {
-        $("#task14").addClass("past"); 
-    } else if (rightNow >= 14 && rightNow < 15 ){
-        $("#task14").addClass("present"); 
-    } else {
-        $("#task14").addClass("future"); 
-    }
-
-    if (rightNow > 15) {
-        $("#task15").addClass("past"); 
-    } else if (rightNow >= 15 && rightNow < 16 ){
-        $("#task15").addClass("present"); 
-    } else {
-        $("#task15").addClass("future"); 
-    }
-
-    if (rightNow > 16) {
-        $("#task16").addClass("past"); 
-    } else if (rightNow >= 16 && rightNow < 17 ){
-        $("#task16").addClass("present"); 
-    } else {
-        $("#task16").addClass("future"); 
-    }
-
-    if (rightNow > 17) {
-        $("#task17").addClass("past"); 
-    } else if (rightNow >= 17 && rightNow < 18 ){
-        $("#task17").addClass("present"); 
-    } else {
-        $("#task17").addClass("future"); 
-    }
-};
-//Calling the function 
-timeFrame();
-
-$(".saveBtn").on("click", function() {
-    // get row text field class and row id values
-    var task = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    console.log(task);
-    // save in localStorage
-    localStorage.setItem(time,task);
+$(document).ready( function() {
+    colorChange ();
+    renderText();
 });
 
-//getting the data from the storage 
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+function colorChange () {    
+    var currentTime = moment().hours();
+    // var currentTime = 13;
+    console.log("Current Time is " + currentTime);       
+// condition to color code the hours 
+    $(".input").each(function () {
+        var scheduledTime = parseInt($(this).attr("id"));
+
+        if (currentTime > scheduledTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (currentTime < scheduledTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
+
+$(".saveBtn").click(function() {
+    var eventText = $(this).siblings(".input").val();
+    // console.log(eventText);
+    var eventTime = $(this).siblings(".hour").text();
+    // console.log(eventTime);
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+
+    colorChange ();
+    renderText ();
+    
+});
+
+
+    // Enter and Display Events
+function renderText () {   
+    var saveEventText9 = JSON.parse(localStorage.getItem("9:00 am"));
+    $("#9").val("");
+    $("#9").val(saveEventText9);
+    
+    var saveEventText10 = JSON.parse(localStorage.getItem("10:00 am"));
+    $("#10").val("");
+    $("#10").val(saveEventText10);
+    
+    var saveEventText11 = JSON.parse(localStorage.getItem("11:00 am"));
+    $("#11").val("");
+    $("#11").val(saveEventText11);
+    
+    var saveEventText12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    $("#12").val("");
+    $("#12").val(saveEventText12);
+    
+    var saveEventText1 = JSON.parse(localStorage.getItem("1:00 pm"));
+    $("#13").val("");
+    $("#13").val(saveEventText1);
+
+    var saveEventText2 = JSON.parse(localStorage.getItem("2:00 pm"));
+    $("#14").val("");
+    $("#14").val(saveEventText2);
+
+    var saveEventText3 = JSON.parse(localStorage.getItem("3:00 pm"));
+    $("#15").val("");
+    $("#15").val(saveEventText3);
+
+    var saveEventText4 = JSON.parse(localStorage.getItem("4:00 pm"));
+    $("#16").val("");
+    $("#16").val(saveEventText4);
+
+    var saveEventText5 = JSON.parse(localStorage.getItem("5:00 pm"));
+    $("#17").val("");
+    $("#17").val(saveEventText5);
+};
